16a54310699904e035167f3773db71e608c08f48
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-29 14:25:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :roster
+
+  def initialize(model)
+    @model = model
+    @roster = {}
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+
+  def add_student(name, grade)
+    # following lines can be written @roster[grade] ||= []
+
+    if @roster[grade] == nil
+       @roster[grade] = []
+    end
+
+    @roster[grade].push(name)
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each{|grade, names|
+      sorted_roster[grade] = names.sort
+    }
+    sorted_roster
+  end
+end
